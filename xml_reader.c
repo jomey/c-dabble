de8db129a089a9063c04a89cdc68b6d0c504bdd7
Joachim Meyer <jomey@uw.edu>
2019-09-13 09:36:30 -0700

    XML reader - Fix indentation.

--- xml_reader.c
+++ xml_reader.c
@@ -26,32 +26,32 @@ static void print_node(xmlNode *node) {
 }
 
 void run_query(const char * query, xmlDocPtr document) {
-    xmlXPathInit();
-    xmlXPathContextPtr ctxt;
-    xmlXPathObjectPtr xpathRes;
-    xmlNodePtr node;
+  xmlXPathInit();
+  xmlXPathContextPtr ctxt;
+  xmlXPathObjectPtr xpathRes;
+  xmlNodePtr node;
 
-    if (NULL == (ctxt = xmlXPathNewContext(document))) {
-      printf("Can't parse document\n");
-      return;
-    }
+  if (NULL == (ctxt = xmlXPathNewContext(document))) {
+    printf("Can't parse document\n");
+    return;
+  }
 
-    if (NULL == (xpathRes = xmlXPathEvalExpression(BAD_CAST query, ctxt))) {
-      printf("Invalid XPath expression\n");
-      return;
-    }
+  if (NULL == (xpathRes = xmlXPathEvalExpression(BAD_CAST query, ctxt))) {
+    printf("Invalid XPath expression\n");
+    return;
+  }
 
-    if (XPATH_NODESET == xpathRes->type) {
-      for (int i = 0; i < xpathRes->nodesetval->nodeNr; i++) {
-        node = xpathRes->nodesetval->nodeTab[i];
-        if (node->type == XML_ELEMENT_NODE) {
-          print_node(node->children);
-        }
+  if (XPATH_NODESET == xpathRes->type) {
+    for (int i = 0; i < xpathRes->nodesetval->nodeNr; i++) {
+      node = xpathRes->nodesetval->nodeTab[i];
+      if (node->type == XML_ELEMENT_NODE) {
+        print_node(node->children);
       }
     }
+  }
 
-    xmlXPathFreeObject(xpathRes);
-    xmlXPathFreeContext(ctxt);
+  xmlXPathFreeObject(xpathRes);
+  xmlXPathFreeContext(ctxt);
 }
 
 int main(int argc, char **argv) {
